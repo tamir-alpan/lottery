510548ea3b10dbe9596b2dff592f34e86ff02ca8
Tamir Alpan <tamiralpan@gmail.com>
2020-06-21 01:19:44 +0300

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+// returns an integer
+function getComputerSelection() {
+    // implement logic
+}
+
+// returns an integer
+function getUserSelection() {
+    // until we learn how to onclick 
+    // change this number manually to check your logic
+    return 0;
+}
+
+// returns a boolean
+function isWin() {
+    // implement logic
+}
+
+var isWin = isWin();
+console.log("isWin: ", isWin) // should return true or false
